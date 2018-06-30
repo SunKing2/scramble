e38682348f1d3002b1d0215a95393c7aba2f0874
SunKing2 <louievbl4t@gmail.com>
2018-06-30 17:15:37 -0400

    moved some importy-module top stuff from main to data store module

--- rust_src/src/main.rs
+++ rust_src/src/main.rs
@@ -3,18 +3,16 @@ extern crate rusqlite;
 extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
-extern crate serde;
+
 mod tcp_server;
 mod data_store;
-
-use rusqlite::Connection;
-
 
 
 fn main() {
     data_store::get_json();
     println!("main...that was json, now passing...");
 
-    //tcp_server::server(&my_json);
+    let my_json = format!("{}", "\"id\" : 10");
+    tcp_server::server(&my_json);
 
 }
